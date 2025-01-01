ba60025a8d5f45153577e3ed9caeb84c795ea20b
tegar-arsya <tegararsya0117@gmail.com>
2025-01-01 23:59:21 +0700

    fix: variable name for create backgrounf

--- pages/api/background/index.ts
+++ pages/api/background/index.ts
@@ -78,7 +78,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               return prisma.background.create({
                 data: {
                   weddingId,
-                  name,
+                  name : name ?? '',
                   gambar: urlPath,
                 },
               });
